165a8b0c2e5d07d17eb2b12607c3b988c4d6c96e
Avinash110 <maheshwariavinash50@gmail.com>
2020-04-08 05:03:54 -0700

    Shortest way to form string updated

--- Strings/1055 Shortest Way to Form String/Solution.py
+++ Strings/1055 Shortest Way to Form String/Solution.py
@@ -1,15 +1,40 @@
+from bisect import bisect_left
+from collections import defaultdict
 class Solution:
-    def shortestWay(self, source: str, target: str) -> int:
-        
-        count = 0
-        targetIndex = 0
-        while targetIndex < len(target):
-            found = False
-            for i in range(len(source)):
-                if targetIndex < len(target) and source[i] == target[targetIndex]:
-                    targetIndex += 1
-                    found = True
-            if not found:
-                return -1
-            count += 1
-        return count
+	def binary_search(self, source: str, target: str) -> int:
+		
+		charIndexes = defaultdict(list)
+		for i, c in enumerate(source):
+			charIndexes[c].append(i)
+		
+		loop_cnt = 1
+		i = 0
+		for t in target:
+			if t not in charIndexes:
+				return -1
+			
+			indexes = charIndexes[t]
+			src_index = bisect_left(indexes, i)
+			if src_index == len(indexes):
+				loop_cnt += 1
+				i = indexes[0] + 1
+			else:
+				i = indexes[src_index] + 1
+		
+		return loop_cnt
+	
+	def shortestWay(self, source: str, target: str) -> int:
+		
+		count = 0
+		targetIndex = 0
+		while targetIndex < len(target):
+			found = False
+			for i in range(len(source)):
+				if targetIndex < len(target) and source[i] == target[targetIndex]:
+					targetIndex += 1
+					found = True
+			if not found:
+				return -1
+			count += 1
+		return count
+
--- Strings/1055 Shortest Way to Form String/test.py
+++ Strings/1055 Shortest Way to Form String/test.py
@@ -5,3 +5,8 @@ assert(sol.shortestWay("abc", "abcbc") == 2)
 assert(sol.shortestWay("abc", "acdbc") == -1)
 assert(sol.shortestWay("xyz", "xzyxz") == 3)
 assert(sol.shortestWay("aaa", "a") == 1)
+
+assert(sol.binary_search("abc", "abcbc") == 2)
+assert(sol.binary_search("abc", "acdbc") == -1)
+assert(sol.binary_search("xyz", "xzyxz") == 3)
+assert(sol.binary_search("aaa", "a") == 1)
