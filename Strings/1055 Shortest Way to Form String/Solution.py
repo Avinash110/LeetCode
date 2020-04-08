ac0443e568e5bdb63d757e4ed6d320006dcd908c
Avinash110 <maheshwariavinash50@gmail.com>
2020-04-08 04:57:09 -0700

    folder structure changes

--- Strings/1055 Shortest Way to Form String/Solution.py
+++ Strings/1055 Shortest Way to Form String/Solution.py
@@ -3,18 +3,13 @@ class Solution:
         
         count = 0
         targetIndex = 0
-        
         while targetIndex < len(target):
-            
             found = False
             for i in range(len(source)):
-                
                 if targetIndex < len(target) and source[i] == target[targetIndex]:
                     targetIndex += 1
                     found = True
-            
             if not found:
                 return -1
-            
             count += 1
         return count
--- make_folder.py
+++ make_folder.py
@@ -1,17 +1,23 @@
 import sys
 import os
+from consolemenu import *
+from consolemenu.items import *
 
 if len(sys.argv) < 2:
-	print("Enter name of the folder")
+	print("Enter name of the Problem")
 	sys.exit(0)
 
-name = sys.argv[1]
+dirs = [x for x in os.listdir('.') if os.path.isdir(x) and x != ".git"]
 
-if not os.path.exists(name):
-    os.makedirs(name)
+folder = dirs[SelectionMenu.get_selection(dirs)]
+name = sys.argv[1]
+problemDir = os.path.join(folder, name)
+
+if not os.path.exists(problemDir):
+    os.makedirs(problemDir)
     
-    solution_file = os.path.join(name, "Solution.py")
-    test_file = os.path.join(name, "test.py")
+    solution_file = os.path.join(problemDir, "Solution.py")
+    test_file = os.path.join(problemDir, "test.py")
 
     with open(solution_file, 'w+') as f:
     	f.write("class Solution:")
